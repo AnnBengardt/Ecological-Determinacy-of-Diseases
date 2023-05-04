1684ba1e6dbd52be7a771c7c789d6cfdcc775813
AnnBengardt <marunko.a@yandex.ru>
2023-05-04 23:19:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ def ml_model():
                 st.write(pd.DataFrame({
                     'Заболевание': list(disease_enc.keys())[1:],
                     'Предрасположенность в %': [str(round(i*100+random.gauss(4, 1.5), 2))+"%" for i in res],
-                }, index=pd.RangeIndex(start=1, stop=5)))
+                }, index=pd.RangeIndex(start=1, stop=6)))
 
 
 def main():
